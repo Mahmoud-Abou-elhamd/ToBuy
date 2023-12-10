e067b29cbaf0e7831b90ca33181249e52a5e3ec1
mahmoud abou-elhamd <mahmoudabohassab00@gmail.com>
2023-12-10 20:38:08 +0200

    added CategoryEntity.kt and Dao and migrated the database to support new table

--- app/src/main/java/com/mahmoud/android/tobuy/database/AppDatabase.kt
+++ app/src/main/java/com/mahmoud/android/tobuy/database/AppDatabase.kt
@@ -4,22 +4,42 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.mahmoud.android.tobuy.database.dao.CategoryEntityDao
 import com.mahmoud.android.tobuy.database.dao.ItemEntityDao
+import com.mahmoud.android.tobuy.database.entity.CategoryEntity
 import com.mahmoud.android.tobuy.database.entity.ItemEntity
 
-@Database(entities = [ItemEntity::class], version = 1)
+@Database(
+    entities = [ItemEntity::class, CategoryEntity::class],
+    version = 2
+)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         private var appDatabase: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase{
-            if(appDatabase != null){
+        fun getDatabase(context: Context): AppDatabase {
+            if (appDatabase != null) {
                 return appDatabase!!
             }
 
-            appDatabase = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "to-buy-database").build()
+            appDatabase = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "to-buy-database"
+            ).addMigrations(Migration_1_2()).build()
+
             return appDatabase!!
         }
     }
+
+    class Migration_1_2 : Migration(1, 2){
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `category_entity` (`id` TEXT NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))")
+        }
+    }
+
     abstract fun itemEntityDao(): ItemEntityDao
+    abstract fun categoryEntityDao(): CategoryEntityDao
 }
---
+++ app/src/main/java/com/mahmoud/android/tobuy/database/dao/CategoryEntityDao.kt
@@ -0,0 +1,20 @@
+package com.mahmoud.android.tobuy.database.dao
+
+import androidx.room.*
+import com.mahmoud.android.tobuy.database.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryEntityDao {
+    @Query("SELECT * FROM category_entity")
+    fun getAllItemEntities(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(categoryEntity: CategoryEntity)
+
+    @Delete
+    suspend fun delete(categoryEntity: CategoryEntity)
+
+    @Update
+    suspend fun update(categoryEntity: CategoryEntity)
+}
